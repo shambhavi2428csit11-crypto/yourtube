ce04e310a390b88d8ba9a7226407e021d01060f7
Shambhavi <shambhavi2428csit11@gmail.com>
2026-08-09 22:56:39 +0530

    add fallback backend url

--- src/lib/axiosinstance.js
+++ src/lib/axiosinstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "https://yourtube-backend-4g9s.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
